44cea6e14417fec82c87cf6b20e1e4bfd5ee05df
aisha1498 <95531968+aisha1498@users.noreply.github.com>
2022-02-26 19:18:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Cab_Invoice_Generator;
+
+Console.WriteLine("Welcome to Cab Invoice Generator");
+InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+double fare = invoiceGenerator.CalculateFare(2.0, 5);
+Console.WriteLine($"Fare : {fare}");
+Console.ReadLine();// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
